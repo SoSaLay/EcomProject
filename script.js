f9c28b5152a1148c3b9cf442fd1f6851c1f94e85
Sulay <Sulaymank704@gmail.com>
2024-05-06 00:09:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+    const hamburger=document.querySelector('.hamburger');
+    const submenu= document.getElementById('submenu');
+
+hamburger.addEventListener('click',function(){
+    
+submenu.classList.toggle('is-visible')
+
+
+
+})
+
+
+
+
+})
