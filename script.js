524184c18b4259e455e7a6321cd886718386284d
Sulay <Sulaymank704@gmail.com>
2024-05-10 14:58:26 -0400

    final

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ navBarLinks.classList.toggle('active')
 })
 
 
-
+// carousel
 document.addEventListener('DOMContentLoaded',function(){
     
 const warpper= document.querySelector('.testimonial-wrapper')
@@ -20,9 +20,7 @@ const leftArrow=document.querySelector('.left-Arrow')
 const rightArrow=document.querySelector('.right-Arrow')
 
 let start=0;
-const delay=100;
-
-
+const delay=2000;
 
 function showNext(){
     start = (start+1) % testimonial.length
@@ -53,7 +51,70 @@ leftArrow.addEventListener('click', function(){
 
 });
 
+})
 
 
+// Form Validation
+
+document.addEventListener('DOMContentLoaded',function(){
+const form= document.getElementById('contactForm');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+    const isFormGood= form.checkValidity();
+
+    isFormGood? window.location.href= 'confirmation.html': alert('Please fill all required fields')
 
 })
+})
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    fetch('products.json')
+    .then(response=>response.json())
+    .then(products=>{
+        const container = document.getElementById('productsContainer');
+        products.forEach(product=>{
+            const box= document.createElement('div');
+            box.classList.add('product-box');
+
+            box.innerHTML = `
+            <img src="${product.image}" alt="${product.name}" class="product-image">
+            <h2 class="product-name">${product.name}</h2>
+            <p class="product-description">${product.description}</p>
+            <p class="product-price">${product.price}</p>
+        `;
+
+        container.appendChild(box);
+        });
+
+    })
+
+    .catch(error=> console.error('Error loading products:',error))
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
